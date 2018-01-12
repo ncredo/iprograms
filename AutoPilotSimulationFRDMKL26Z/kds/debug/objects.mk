0ceae0d44e18e5b7daf19902b581a77236758cd2
NDUHURA <credo.nduhura@gmail.com>
2018-01-11 22:13:58 -0500

    adding files

--- AutoPilotSimulationFRDMKL26Z/kds/debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Freescale/KSDK_1.3.0/examples/frdmkl26z/demo_apps/bubble_level_tpm/kds/../../../../../lib/ksdk_platform_lib/kds/KL26Z4/debug/libksdk_platform.a
-
-LIBS := -lm -lg -lgcc -lnosys
-
--- AutoPilotSimulationFRDMKL26Z/kds/debug/utilities/print_scan.d
+++
@@ -1,5 +0,0 @@
-utilities/print_scan.o: \
- C:/Freescale/KSDK_1.3.0/platform/utilities/src/print_scan.c \
- C:/Freescale/KSDK_1.3.0/platform/utilities/src/print_scan.h
-
-C:/Freescale/KSDK_1.3.0/platform/utilities/src/print_scan.h:
